a3f466997bf646bb08f84bb7d8c0ac3d2176b5f5
anshuman_N <forgitonlyanshuman@gmail.com>
2025-08-24 18:30:43 +0530

    add

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "aws-project-terraform-state"
+    bucket = "aws-project-terraform-state-file"
     key    = "terraform.tfstate"
     region = "ap-south-1"
   }
