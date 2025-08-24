e0927f18f512592634bcbacd8293b77d7cb74328
anshuman_N <forgitonlyanshuman@gmail.com>
2025-08-24 20:23:57 +0530

    ad

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "aws-project-terraform-state"   # must exist already
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
 # Website hosting bucket
 resource "aws_s3_bucket" "website" {
   bucket = "aws-project-website-code-bucket"
